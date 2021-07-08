d727b48919fad24cf4bae7fc6a3d3d5ebef5717b
Alessandro  Cudazzo <alessandro@cudazzo.com>
2021-07-08 13:29:38 +0200

    Added report

--- src/rest_bilstm/a_bilstm.py
+++ src/rest_bilstm/a_bilstm.py
@@ -1,31 +1,38 @@
-from tensorflow.keras.layers import Layer
-from tensorflow.keras import backend as K
+import tensorflow as tf
 
-class Attention(Layer):
-    
-    def __init__(self, return_sequences=True):
+class Attention(tf.keras.layers.Layer):
+
+    def __init__(self, return_sequences=True, name=None, **kwargs):
+        super(Attention, self).__init__(name=name)
         self.return_sequences = return_sequences
-        super(Attention,self).__init__()
-        
+        super(Attention, self).__init__(**kwargs)
+
     def build(self, input_shape):
-        
+    
         self.W=self.add_weight(name="att_weight", shape=(input_shape[-1],1),
-                               initializer="normal")
+                           initializer="glorot_uniform", trainable=True)
         self.b=self.add_weight(name="att_bias", shape=(input_shape[1],1),
-                               initializer="zeros")
-        
-        super(Attention,self).build(input_shape)
-        
+                           initializer="zeros", trainable=True)
+    
+        super(Attention, self).build(input_shape)
+
     def call(self, x):
-        
-        e = K.tanh(K.dot(x,self.W)+self.b)
-        a = K.softmax(e, axis=1)
-        output = x*a
-        
+    
+        e = tf.keras.activations.tanh(tf.keras.backend.dot(x, self.W) + self.b)
+        a = tf.keras.activations.softmax(e, axis=1)
+        output = x * a
+    
         if self.return_sequences:
-            return output
-        
-        return K.sum(output, axis=1)
+            return a, output
+    
+        return a, tf.keras.backend.sum(output, axis=1)
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({
+            'return_sequences': self.return_sequences 
+        })
+        return config
 
 def a_bilstm(input_shape, input_shape_pos, bi_units = 256, bi_dropout = 0.5, dropout = 0.5, hn=128, lr=0.01, print_model=False):
     import tensorflow as tf
